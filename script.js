fdca5da5930892e346275ca432f50fa8339f56d9
Ivan Shakin <ivanxshakin1993@gmail.com>
2024-11-06 16:00:08 +0300

    minor changes made

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let select__language = document.querySelector('.select__language');
 let welcomeText = document.querySelector('.welcoming');
 let changeTheme = document.querySelector('.theme');
 let mainTheme = document.querySelector('.main');
+let changeImage = document.getElementById('manImage')
 let contentText = document.querySelector('.content__text');
 
 changeTheme.addEventListener("click", function () {
@@ -18,6 +19,8 @@ russian.addEventListener('click', function (e) {
     select__language.textContent = 'Выберите язык'
     contentText.textContent = 'Ищете идеальный смартфон? Мы предлагаем широкий выбор новых и сертифицированных айфонов по лучшим ценам! Откройте для себя передовые технологии, стильный дизайн и надежную работу – все это в одном устройстве.'
     changeTheme.textContent = 'Сменить тему';
+    changeImage.src = 'photos/man-1.png';
+    changeImage.style.height = '400px';
 });
 
 english.addEventListener('click', function (e) {
@@ -25,6 +28,7 @@ english.addEventListener('click', function (e) {
     select__language.textContent = 'Select the language'
     contentText.textContent = 'Are you looking for the perfect smartphone? We offer a wide selection of new and certified iPhones at the best prices! Discover cutting-edge technology, stylish design, and reliable performance – all in one device.'
     changeTheme.textContent = 'Change theme';
+    changeImage.src = 'photos/man-2.png';
 });
 
 spanish.addEventListener('click', function (e) {
@@ -32,6 +36,7 @@ spanish.addEventListener('click', function (e) {
     select__language.textContent = 'Elija la lengua';
     contentText.textContent = '¿Estás buscando el smartphone perfecto? ¡Ofrecemos una amplia selección de iPhones nuevos y certificados a los mejores precios! Descubre la tecnología de vanguardia, un diseño elegante y un rendimiento confiable, todo en un solo dispositivo.'
     changeTheme.textContent = 'Cambiar color';
+    changeImage.src = 'photos/man-3.jpg';
 });
 
 
